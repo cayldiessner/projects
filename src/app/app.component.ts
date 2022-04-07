e6f1f8360f0c6927b93da0e65fb62bbe2b14796f
Cayl Diessner <Cayl.Diessner@4rhuskies.org>
2022-04-07 09:55:38 -0500

    'hw2-1'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,8 @@ export class AppComponent {
     { title: 'Test Average Hw1', url: '/folder/hw1_10/hw1-prob10', icon: 'analytics'},
     { title: 'MPG Hw1', url: '/folder/hw1_9/hw1prob9', icon: 'car-sport'},
     { title: 'String Manipulator Hw1', url: '/folder/hw1_12/hw1prob12', icon: 'business'},
-
+    { title: 'Magic 8 Ball', url: '/folder/magic8ball/magic8ball', icon: 'ellipse'},
+    { title: 'Roman Numerals', url: '/folder//', icon: ''},
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
